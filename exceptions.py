79a7cf1b1955b45b5039b512ce14581d4453c658
Rohan <rohankanchana@github.com>
2020-03-30 00:52:26 -0400

    sheets setup

---
+++ exceptions.py
@@ -0,0 +1,3 @@
+
+class NoDataFoundError(Exception):
+	pass 
---
+++ sample_config.py
@@ -0,0 +1,7 @@
+# Sample config - to use with other scripts, fill out and rename 'config.py'.
+
+SPREADSHEET_ID: str = 'The unique ID of your Google Sheets'
+
+SHEET_NAME: str = 'The name of the specific sheet you are querying (since a single spreadsheet can have multiple sheets).'
+
+SHEET_RANGE: str = 'The range within the sheet you want to query.'
