3bd0a4d84cf2c08591cf854f6528df87aede5ad1
Rohan <rohankanchana@github.com>
2020-04-26 01:33:26 -0400

    django wsgi server

--- html_builder.py
+++ html_builder.py
@@ -7,9 +7,9 @@ TEMPLATE_FILE = 'template.html'
 
 
 def render_template(**kwargs):
-	loader = jinja2.FileSystemLoader(searchpath='./')
-	env = jinja2.Environment(loader=loader)
-	template = env.get_template(TEMPLATE_FILE)
-	html = template.render(**kwargs)
+    loader = jinja2.FileSystemLoader(searchpath='./')
+    env = jinja2.Environment(loader=loader)
+    template = env.get_template(TEMPLATE_FILE)
+    html = template.render(**kwargs)
 
-	return html
+    return html
